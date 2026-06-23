f2937ea3ee3ab39e031f4623a0d75b01b80995b5
Kamran Ahmed <kamranahmed.se@gmail.com>
2026-06-23 12:05:16 +0100

    Fix home page examples not firing

--- docs/src/components/CodeSample.tsx
+++ docs/src/components/CodeSample.tsx
@@ -1,6 +1,7 @@
 import type { Config, DriveStep, PopoverDOM } from "driver.js";
 import { driver } from "driver.js";
 import "driver.js/dist/driver.css";
+import { mountDummyElement, removeDummyElement } from "../lib/dummyElement";
 
 type CodeSampleProps = {
   heading?: string;
@@ -14,30 +15,6 @@ type CodeSampleProps = {
   children?: any;
   buttonText?: string;
 };
-
-export function removeDummyElement() {
-  const el = document.querySelector(".dynamic-el");
-  if (el) {
-    el.remove();
-  }
-}
-
-export function mountDummyElement() {
-  const newDiv = (document.querySelector(".dynamic-el") || document.createElement("div")) as HTMLElement;
-
-  newDiv.innerHTML = "This is a new Element";
-  newDiv.style.display = "block";
-  newDiv.style.padding = "20px";
-  newDiv.style.backgroundColor = "black";
-  newDiv.style.color = "white";
-  newDiv.style.fontSize = "14px";
-  newDiv.style.position = "fixed";
-  newDiv.style.top = `${Math.random() * (500 - 30) + 30}px`;
-  newDiv.style.left = `${Math.random() * (500 - 30) + 30}px`;
-  newDiv.className = "dynamic-el";
-
-  document.body.appendChild(newDiv);
-}
 
 function attachFirstButton(popover: PopoverDOM) {
   const firstButton = document.createElement("button");
